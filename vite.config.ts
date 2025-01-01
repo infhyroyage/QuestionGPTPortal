0755416047a26a4ceb15ca78424616a914b48e8a
Takeru KIDO <keidy2463@gmail.com>
2025-01-01 14:40:23 +0900

    Fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/QuestionGPTPortal/" : "/",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+}));
