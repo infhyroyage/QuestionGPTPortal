42a84b19dbea10c8b634b5b993a9f40d58d3b87d
Takeru KIDO <keidy2463@gmail.com>
2025-01-08 00:23:52 +0900

    Display courses and tests in RootPage

--- src/pages/RootPage.tsx
+++ src/pages/RootPage.tsx
@@ -3,15 +3,11 @@ import TopBar from "@/components/TopBar";
 import { Button } from "@/components/ui/button";
 import { useToast } from "@/hooks/ui/use-toast";
 import { accessBackend } from "@/lib/backend";
-import { GetTests } from "@/types/backend";
+import { GetTests, Test } from "@/types/backend";
 import { useAccount, useMsal } from "@azure/msal-react";
-import {
-  ChevronDown,
-  ChevronUp,
-  ScrollText,
-  TriangleAlert,
-} from "lucide-react";
-import { useEffect, useState } from "react";
+import { ChevronDown, ScrollText, TriangleAlert } from "lucide-react";
+import { Fragment, useEffect, useState } from "react";
+import { useNavigate } from "react-router";
 
 /**
  * ルートページのコンポーネント
@@ -19,14 +15,14 @@ import { useEffect, useState } from "react";
  */
 export default function RootPage() {
   const [getTests, setGetTests] = useState<GetTests | undefined>(undefined);
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [opens, setOpens] = useState<boolean[]>([]);
 
   const { toast } = useToast();
 
   const { instance, accounts } = useMsal();
   const accountInfo = useAccount(accounts[0] || {});
 
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
   // 初回レンダリング時のみ[GET] /testsを実行
   useEffect(() => {
@@ -39,6 +35,7 @@ export default function RootPage() {
           accountInfo
         );
 
+        setOpens(Array(Object.keys(res).length).fill(false));
         setGetTests(res);
       } catch (e) {
         console.error(e);
@@ -68,42 +65,50 @@ export default function RootPage() {
             <div>テストが見つかりませんでした</div>
           </div>
         ) : (
-          <>
-            <Button
-              variant="ghost"
-              className="px-6 py-8 w-full justify-start space-x-4"
-              onClick={() => setIsOpen(!isOpen)}
-            >
-              {isOpen ? (
-                <ChevronUp className="h-6 w-6" />
-              ) : (
-                <ChevronDown className="h-6 w-6" />
+          Object.keys(getTests).map((course: string, i: number) => (
+            <Fragment key={i}>
+              <Button
+                variant="ghost"
+                className="px-6 py-8 w-full justify-start space-x-4"
+                onClick={() => {
+                  setOpens((prev) => [
+                    ...prev.slice(0, i),
+                    !prev[i],
+                    ...prev.slice(i + 1),
+                  ]);
+                }}
+              >
+                <ChevronDown
+                  className={`h-6 w-6 transform transition-transform duration-200 ${
+                    opens[i] ? "rotate-180" : "rotate-0"
+                  }`}
+                />
+                <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+                  {course}
+                </h3>
+              </Button>
+              {opens[i] && (
+                <div className="pl-16 space-y-2">
+                  {getTests[course].map((test: Test) => (
+                    <div
+                      key={test.id}
+                      className="flex items-center justify-start space-x-4"
+                    >
+                      <ScrollText className="h-5 w-5" />
+                      <Button
+                        variant="link"
+                        onClick={() => navigate(`/tests/${test.id}/ready`)}
+                      >
+                        <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+                          {test.testName}
+                        </h4>
+                      </Button>
+                    </div>
+                  ))}
+                </div>
               )}
-              <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
-                XXX
-              </h3>
-            </Button>
-            {isOpen && (
-              <div className="pl-16 space-y-2">
-                <div className="flex items-center justify-start space-x-4">
-                  <ScrollText className="h-5 w-5" />
-                  <Button variant="link">
-                    <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
-                      YYY
-                    </h4>
-                  </Button>
-                </div>
-                <div className="flex items-center justify-start space-x-4">
-                  <ScrollText className="h-5 w-5" />
-                  <Button variant="link">
-                    <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
-                      ZZZ
-                    </h4>
-                  </Button>
-                </div>
-              </div>
-            )}
-          </>
+            </Fragment>
+          ))
         )}
       </div>
     </>
