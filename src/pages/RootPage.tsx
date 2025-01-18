10b6722ddd95d6b6cc52e15f4454e7a71f1e32a3
Takeru KIDO <keidy2463@gmail.com>
2025-01-18 23:10:49 +0900

    Add Skeleton in RootPage

--- src/pages/RootPage.tsx
+++ src/pages/RootPage.tsx
@@ -1,12 +1,12 @@
-import LoadingCenter from "@/components/LoadingCenter";
 import TopBar from "@/components/TopBar";
 import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 import { useToast } from "@/hooks/ui/use-toast";
 import { accessBackend } from "@/lib/backend";
 import { GetTests, Test } from "@/types/backend";
 import { useAccount, useMsal } from "@azure/msal-react";
 import { ChevronDown, ScrollText, TriangleAlert } from "lucide-react";
-import { Fragment, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 
 /**
@@ -58,57 +58,63 @@ export default function RootPage() {
       <TopBar title="Question GPT Portal" />
       <div className="pt-16">
         {!getTests ? (
-          <LoadingCenter />
+          <div className="space-y-2">
+            <Skeleton className="px-6 py-8 w-full rounded-md" />
+            <Skeleton className="px-6 py-8 w-full rounded-md" />
+            <Skeleton className="px-6 py-8 w-full rounded-md" />
+          </div>
         ) : Object.keys(getTests).length === 0 ? (
           <div className="flex items-center justify-center h-screen flex-col gap-4">
             <TriangleAlert size={100} />
             <div>テストが見つかりませんでした</div>
           </div>
         ) : (
-          Object.keys(getTests).map((course: string, i: number) => (
-            <Fragment key={i}>
-              <Button
-                variant="ghost"
-                className="px-6 py-8 w-full justify-start space-x-4"
-                onClick={() => {
-                  setOpens((prev) => [
-                    ...prev.slice(0, i),
-                    !prev[i],
-                    ...prev.slice(i + 1),
-                  ]);
-                }}
-              >
-                <ChevronDown
-                  className={`h-6 w-6 transform transition-transform duration-200 ${
-                    opens[i] ? "rotate-180" : "rotate-0"
-                  }`}
-                />
-                <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
-                  {course}
-                </h3>
-              </Button>
-              {opens[i] && (
-                <div className="pl-16 space-y-2">
-                  {getTests[course].map((test: Test) => (
-                    <div
-                      key={test.id}
-                      className="flex items-center justify-start space-x-4"
-                    >
-                      <ScrollText className="h-5 w-5" />
-                      <Button
-                        variant="link"
-                        onClick={() => navigate(`/tests/${test.id}/ready`)}
+          <div className="space-y-2">
+            {Object.keys(getTests).map((course: string, i: number) => (
+              <div key={i}>
+                <Button
+                  variant="ghost"
+                  className="px-6 py-8 w-full justify-start space-x-4"
+                  onClick={() => {
+                    setOpens((prev) => [
+                      ...prev.slice(0, i),
+                      !prev[i],
+                      ...prev.slice(i + 1),
+                    ]);
+                  }}
+                >
+                  <ChevronDown
+                    className={`h-6 w-6 transform transition-transform duration-200 ${
+                      opens[i] ? "rotate-180" : "rotate-0"
+                    }`}
+                  />
+                  <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+                    {course}
+                  </h3>
+                </Button>
+                {opens[i] && (
+                  <div className="pl-16 space-y-2">
+                    {getTests[course].map((test: Test) => (
+                      <div
+                        key={test.id}
+                        className="flex items-center justify-start space-x-4"
                       >
-                        <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
-                          {test.testName}
-                        </h4>
-                      </Button>
-                    </div>
-                  ))}
-                </div>
-              )}
-            </Fragment>
-          ))
+                        <ScrollText className="h-5 w-5" />
+                        <Button
+                          variant="link"
+                          onClick={() => navigate(`/tests/${test.id}/ready`)}
+                        >
+                          <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+                            {test.testName}
+                          </h4>
+                        </Button>
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
         )}
       </div>
     </>
