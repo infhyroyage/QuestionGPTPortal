a2f9e10e82135fefa9f5cd51e0f680f432e67c58
Takeru KIDO <keidy2463@gmail.com>
2025-02-03 23:23:00 +0900

    Scroll ExplainationSheet

---
+++ src/components/ExplanationSheet.tsx
@@ -0,0 +1,51 @@
+import {
+  fetchAnswerExplanationAtom,
+  fetchTranslationExplanationAtom,
+} from "@/lib/atoms";
+import { useAtom } from "jotai";
+import { Skeleton } from "./ui/skeleton";
+
+/**
+ * 解説シートのコンポーネント
+ * @returns 解説シートのコンポーネント
+ */
+export default function ExplanationSheet() {
+  const [answerExplanation] = useAtom(fetchAnswerExplanationAtom);
+  const [translationExplanation] = useAtom(fetchTranslationExplanationAtom);
+
+  return (
+    <>
+      <h4 className="scroll-m-20 text-xl font-semibold tracking-tight my-4">
+        解説
+      </h4>
+      <div className="space-y-4 mb-4">
+        {answerExplanation && answerExplanation.explanations ? (
+          answerExplanation.explanations.map((explanation, idx) => (
+            <div key={idx} className="space-y-1">
+              <p className="leading-7">{explanation}</p>
+              {translationExplanation &&
+              translationExplanation.explanations[idx] ? (
+                <p className="text-sm text-muted-foreground">
+                  {translationExplanation.explanations[idx]}
+                </p>
+              ) : (
+                <Skeleton className="h-5 w-full" />
+              )}
+            </div>
+          ))
+        ) : (
+          <>
+            <div className="space-y-1">
+              <Skeleton className="h-7 w-full" />
+              <Skeleton className="h-5 w-full" />
+            </div>
+            <div className="space-y-1">
+              <Skeleton className="h-7 w-full" />
+              <Skeleton className="h-5 w-full" />
+            </div>
+          </>
+        )}
+      </div>
+    </>
+  );
+}
--- src/components/OpenExplanationButton.tsx
+++ src/components/OpenExplanationButton.tsx
@@ -1,13 +1,10 @@
-import {
-  fetchAnswerExplanationAtom,
-  fetchTranslationExplanationAtom,
-} from "@/lib/atoms";
+import { fetchAnswerExplanationAtom } from "@/lib/atoms";
 import { useAtom } from "jotai";
 import { ArrowLeftFromLine } from "lucide-react";
 import { useMemo } from "react";
+import ExplanationSheet from "./ExplanationSheet";
 import { Button } from "./ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
-import { Skeleton } from "./ui/skeleton";
 
 /**
  * 解説表示ボタンのコンポーネント
@@ -15,7 +12,7 @@ import { Skeleton } from "./ui/skeleton";
  */
 export default function OpenExplanationButton() {
   const [answerExplanation] = useAtom(fetchAnswerExplanationAtom);
-  const [translationExplanation] = useAtom(fetchTranslationExplanationAtom);
+
   // 回答・解説を生成していない場合は、解説表示ボタンを非活性とする
   const isDisabledOpenExplanationButton = useMemo<boolean>(
     () => !answerExplanation || !answerExplanation.explanations,
@@ -29,38 +26,8 @@ export default function OpenExplanationButton() {
           <ArrowLeftFromLine />
         </Button>
       </SheetTrigger>
-      <SheetContent className="min-w-[40vw]">
-        <h4 className="scroll-m-20 text-xl font-semibold tracking-tight my-4">
-          解説
-        </h4>
-        <div className="space-y-4 mb-4">
-          {answerExplanation && answerExplanation.explanations ? (
-            answerExplanation.explanations.map((explanation, idx) => (
-              <div key={idx} className="space-y-1">
-                <p className="leading-7">{explanation}</p>
-                {translationExplanation &&
-                translationExplanation.explanations[idx] ? (
-                  <p className="text-sm text-muted-foreground">
-                    {translationExplanation.explanations[idx]}
-                  </p>
-                ) : (
-                  <Skeleton className="h-5 w-full" />
-                )}
-              </div>
-            ))
-          ) : (
-            <>
-              <div className="space-y-1">
-                <Skeleton className="h-7 w-full" />
-                <Skeleton className="h-5 w-full" />
-              </div>
-              <div className="space-y-1">
-                <Skeleton className="h-7 w-full" />
-                <Skeleton className="h-5 w-full" />
-              </div>
-            </>
-          )}
-        </div>
+      <SheetContent className="min-w-[40vw] max-h-[100vh] overflow-y-auto">
+        <ExplanationSheet />
       </SheetContent>
     </Sheet>
   );
