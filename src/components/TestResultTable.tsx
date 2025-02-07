4a52e790dfd848dfabe99e56b04e1428798a326e
Takeru KIDO <keidy2463@gmail.com>
2025-02-07 19:55:02 +0900

    Add expanding rows of TestResultTable

--- src/components/TestResultTable.tsx
+++ src/components/TestResultTable.tsx
@@ -9,12 +9,25 @@ import {
 import { TestResultTableProps } from "@/types/props";
 import { ProgressTestHistory } from "@/types/storage";
 import { Check, X } from "lucide-react";
+import { Fragment, useCallback, useState } from "react";
 
 /**
  * テスト結果テーブルのコンポーネント
  * @returns テスト結果テーブルのコンポーネント
  */
 export default function TestResultTable({ histories }: TestResultTableProps) {
+  const [opens, setOpens] = useState<boolean[]>(
+    [...Array(histories.length)].fill(false)
+  );
+
+  const onClick = useCallback((idx: number) => {
+    setOpens((prev) => {
+      const newOpens = [...prev];
+      newOpens[idx] = !newOpens[idx];
+      return newOpens;
+    });
+  }, []);
+
   return (
     <Table>
       <TableHeader>
@@ -25,16 +38,28 @@ export default function TestResultTable({ histories }: TestResultTableProps) {
       </TableHeader>
       <TableBody>
         {histories.map((history: ProgressTestHistory, idx: number) => (
-          <TableRow key={idx}>
-            <TableCell className="font-medium">{idx + 1}</TableCell>
-            <TableCell>
-              {history.isCorrect ? (
-                <Check className="text-green-500" />
-              ) : (
-                <X className="text-red-500" />
-              )}
-            </TableCell>
-          </TableRow>
+          <Fragment key={idx}>
+            <TableRow
+              onClick={() => onClick(idx)}
+              className={`cursor-pointer${
+                opens[idx] ? " rounded-t-md border-b-0" : ""
+              }`}
+            >
+              <TableCell className="font-medium">{idx + 1}</TableCell>
+              <TableCell>
+                {history.isCorrect ? (
+                  <Check className="text-green-500" />
+                ) : (
+                  <X className="text-red-500" />
+                )}
+              </TableCell>
+            </TableRow>
+            {opens[idx] && (
+              <TableRow className="rounded-b-md border-t-0">
+                <TableCell colSpan={2}>TODO</TableCell>
+              </TableRow>
+            )}
+          </Fragment>
         ))}
       </TableBody>
     </Table>
