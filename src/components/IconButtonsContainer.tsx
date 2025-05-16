c806e801bc21c8b6d3ca70c21a03bf7c6b44b415
Takeru KIDO <keidy2463@gmail.com>
2025-05-16 23:04:40 +0900

    Update IconButtonsContainer UI

--- src/components/IconButtonsContainer.tsx
+++ src/components/IconButtonsContainer.tsx
@@ -1,3 +1,6 @@
+import { fetchQuestionSelectorAtom } from "@/lib/atoms";
+import { useAtomValue } from "jotai";
+import { useMemo } from "react";
 import NextQuestionButton from "./NextQuestionButton";
 import OpenExplanationButton from "./OpenExplanationButton";
 import ResubmitButton from "./ResubmitButton";
@@ -8,16 +11,27 @@ import SubmitButton from "./SubmitButton";
  * @returns TestQuestionPageのアイコンボタン群を配置するコンテナ
  */
 export default function IconButtonsContainer() {
+  const questionSelector = useAtomValue(fetchQuestionSelectorAtom);
+
+  // 選択肢がいずれも選択していない場合は、アイコンボタン群を表示しない
+  const isHidden = useMemo<boolean>(
+    () =>
+      !questionSelector ||
+      questionSelector.choices.every((choice) => !choice.isSelected),
+    [questionSelector]
+  );
+
   return (
-    <div className="absolute bottom-4 right-4 flex flex-col space-y-4">
-      <div className="flex space-x-4">
-        <SubmitButton />
-        <OpenExplanationButton />
-      </div>
-      <div className="flex space-x-4">
-        <ResubmitButton />
-        <NextQuestionButton />
-      </div>
+    <div
+      className={`space-x-4 absolute right-4 transition-all duration-300 ease-in-out ${
+        isHidden ? "translate-y-20 opacity-0" : "bottom-4 opacity-100"
+      }
+      `}
+    >
+      <SubmitButton />
+      <ResubmitButton />
+      <OpenExplanationButton />
+      <NextQuestionButton />
     </div>
   );
 }
