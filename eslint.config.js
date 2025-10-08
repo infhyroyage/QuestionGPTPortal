6c9129359c58e9199bda609c16a22107f627d742
Takeru KIDO <keidy2463@gmail.com>
2025-10-09 00:41:00 +0900

    Update deprecated method

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,15 @@
 import js from "@eslint/js";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import { defineConfig } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
+export default defineConfig(
   { ignores: ["dist", "src/components/ui", "src/hooks/ui", "src/lib"] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
